61287d33a2997c9a98ab88771f01fdc53c08314e
lizuoyang <lizuoyangsina@qq.com>
2022-06-08 11:48:15 +0800

    feat: 添加自定义配置类

---
+++ lzy-platform-mybatis/src/main/java/com/lzy/platform/mybatis/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.lzy.platform.mybatis.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName MybatisPlusConfig
+ * @Description MyBatis-Plus自定义配置
+ * @Author LiZuoYang
+ * @Date 2022/6/8 11:10
+ **/
+@Configuration
+@MapperScan("com.lzy.**.mapper.**")
+public class MybatisPlusConfig {
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
