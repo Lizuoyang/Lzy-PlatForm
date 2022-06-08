a66c2eea11f41152e0ec531823d159fc569c1e46
lizuoyang <lizuoyangsina@qq.com>
2022-06-08 14:29:52 +0800

    feat: 引入swagger-knife4j模块

--- lzy-platform-mybatis/src/main/java/com/lzy/platform/mybatis/config/MybatisPlusConfig.java
+++ lzy-platform-mybatis/src/main/java/com/lzy/platform/mybatis/config/MybatisPlusConfig.java
@@ -3,7 +3,6 @@ package com.lzy.platform.mybatis.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
  * @Date 2022/6/8 11:10
  **/
 @Configuration
-@MapperScan("com.lzy.**.mapper.**")
 public class MybatisPlusConfig {
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false
