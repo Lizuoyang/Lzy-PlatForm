4bf0f25e6830d191f18ffbb6ff2b5fc86b8de615
lizuoyang <lizuoyangsina@qq.com>
2022-06-30 17:39:54 +0800

    feat: 添加Feign自定义拦截请求拦截器

--- lzy-platform-springcloud/src/main/java/com/lzy/platform/ribbon/sentinel/handler/LzyCloudBlockExceptionHandler.java
+++ lzy-platform-springcloud/src/main/java/com/lzy/platform/ribbon/sentinel/handler/LzyCloudBlockExceptionHandler.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lzy.platform.base.result.Result;
 import com.lzy.platform.base.enums.ResultCodeEnum;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,11 +21,25 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 @Component
 public class LzyCloudBlockExceptionHandler implements BlockExceptionHandler {
+    /**
+     * 服务名
+     */
+    @Value("${spring.application.name}")
+    private String serverName;
+
+    /**
+     * 系统繁忙信息
+     */
+    private String errorSystemMsg;
+
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, BlockException exception) throws Exception {
+    public void handle(HttpServletRequest request, HttpServletResponse response, BlockException e) throws Exception {
         response.setStatus(ResultCodeEnum.SYSTEM_BUSY.getCode());
         response.setContentType("application/json;charset=utf-8");
-        Result result = Result.error(ResultCodeEnum.SYSTEM_BUSY, ResultCodeEnum.SYSTEM_BUSY.getMsg());
+        this.errorSystemMsg = new StringBuffer()
+                .append(this.serverName)
+                .append(": ").append(ResultCodeEnum.SYSTEM_BUSY.getMsg()).toString();
+        Result result = Result.error(ResultCodeEnum.SYSTEM_BUSY, this.errorSystemMsg);
         new ObjectMapper().writeValue(response.getWriter(), result);
     }
 }
